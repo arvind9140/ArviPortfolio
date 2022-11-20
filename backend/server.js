d47264ba71ebea497e41a8604b99beca323866ca
arvind9140 <ar789maurya@gmail.com>
2022-11-20 17:52:58 +0530

    removed dirname from line 23

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,8 @@ import { app} from "./app.js"
 import dotenv from "dotenv"
 import { connectDatabase } from "./config/database.js";
 import cloudinary from "cloudinary";
-// import express from 'express'
-// import path from 'path'
+import express from 'express'
+import path from 'path'
 
 // require('dotenv').config({path:"./config/.env"})
 dotenv.config({path:"./backend/config/.env"});
@@ -16,6 +16,8 @@ cloudinary.v2.config({
   api_secret: 'PDhOv_RVTqE_G4txGp6W_ubLwzw' 
 });
 
+process.env.PWD = process.cwd();
+
 // server production asserts
 
 // if(process.env.NODE_ENV === "production"){
@@ -25,6 +27,15 @@ cloudinary.v2.config({
 //   res.sendFile(__dirname + '/index.html');
 // });
 // }
+process.env.PWD = process.cwd();
+
+app.use(express.static(process.env.PWD + '/build'));
+
+app.get('*', function (req, res) {
+  const index = path.join(process.env.PWD, '/frontend/build/index.html');
+  res.sendFile(index);
+});
+
 
 app.listen(process.env.PORT, ()=>{
   console.log(`Server is running on port: ${process.env.PORT}`)
