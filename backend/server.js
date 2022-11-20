ece6e54bb41d0e4cff6771b4032f1c8d0484a304
arvind9140 <ar789maurya@gmail.com>
2022-11-20 17:31:28 +0530

    removed dirname from line 23

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ if(process.env.NODE_ENV === "production"){
   app.use(express.static(path.join("frontend/build")))
   app.get('*', function (req, res) {
     const index = path.join( 'build', 'index.html');
-    res.sendFile(index);
+    res.sendFile('index.html');
   });
 }
 
