a76233985afb86e083fff7aeb0b072b509eac0e4
arvind9140 <ar789maurya@gmail.com>
2022-11-21 14:45:09 +0530

    removed dirname from line 23

--- backend/server.js
+++ backend/server.js
@@ -24,8 +24,8 @@ cloudinary.v2.config({
 if(process.env.NODE_ENV === "production"){
   app.use(express.static(path.join("frontend/build")))
  app.get('*', function (req, res) {
-  const index = path.join('frontend', 'build', 'index.html');
-  res.sendFile('index.html');
+  const index = path.join( 'frontend', 'build', 'index.html');
+  res.sendFile('index.html', { path: __dirname });
 });
 }
 // process.env.PWD = process.cwd();
