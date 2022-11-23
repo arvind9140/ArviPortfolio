d9a839bc64ec5f4e4923daee8eae9c702a40adea
arvind9140 <ar789maurya@gmail.com>
2022-11-23 12:05:38 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ if(process.env.NODE_ENV === "production"){
   app.use(express.static(path.join("frontend/build")));
   app.get('*', function (req, res) {
     const index = path.join( 'frontend', 'build', 'index.html');
-    res.sendFile('index.html', { path: __dirname });
+    res.sendFile(path.resolve((__dirname, "frontend", "build", "index.html")));
   });
 }
 
