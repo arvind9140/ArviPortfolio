a66454da289ed2db0aade8d56ca2b8796211aa23
arvind9140 <ar789maurya@gmail.com>
2022-11-23 13:26:04 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,10 @@ import { connectDatabase } from "./config/database.js";
 import cloudinary from "cloudinary";
 import express from 'express'
 import path from 'path'
-import { dirname } from "path"; 
-
-
-
+// import { dirname } from "path"; 
+// const  __dirname = path.resolve(dirname);
+// require('dotenv').config({path:"./config/.env"})
+dotenv.config({path:"./backend/config/.env"});
 
 connectDatabase();
 
